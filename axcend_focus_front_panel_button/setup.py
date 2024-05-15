835320883e387090be43a4a16be56e4e9fb56e7c
Graham Harrison <graham.harrison@axcendcorp.com>
2024-05-15 12:35:00 -0600

    Rebase from 'upstream'

--- axcend_focus_front_panel_button/setup.py
+++ axcend_focus_front_panel_button/setup.py
@@ -4,7 +4,7 @@ package_name = 'axcend_focus_front_panel_button'
 
 setup(
     name=package_name,
-    version='3.1.2',
+    version='3.1.3',
     packages=[package_name],
     data_files=[
         ('share/ament_index/resource_index/packages',
