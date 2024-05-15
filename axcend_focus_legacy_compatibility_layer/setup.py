035dfa8f0b166e5e831396f10d77fbae16e9f56c
Graham Harrison <graham.harrison@axcendcorp.com>
2024-05-15 12:36:10 -0600

    Revert "Trimmed the branch to only the axcend_focus_test_utils_package sub directory"
    This reverts commit c9aab1b318bf2205c2eeadcfc0c51b851253b0a3.

---
+++ axcend_focus_legacy_compatibility_layer/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+package_name = 'axcend_focus_legacy_compatibility_layer'
+
+setup(
+    name=package_name,
+    version='3.1.2',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools', 'flask'],
+    zip_safe=True,
+    maintainer='ubuntu',
+    maintainer_email='graham.harrison@axcendcorp.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'legacy_compatibility_interface = axcend_focus_legacy_compatibility_layer.legacy_compatibility_interface:main'
+        ],
+    },
+)
