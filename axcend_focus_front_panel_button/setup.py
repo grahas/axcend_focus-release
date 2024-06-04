1b00d3cd1420d03873eb7a3d9b9e8e6adcda7a3b
Graham Harrison <graham.harrison@axcendcorp.com>
2024-06-04 11:45:43 -0600

    Revert "Trimmed the branch to only the axcend_focus_legacy_compatibility_layer sub directory"
    This reverts commit f5ca378248bcd6133f15369f070a788c66856fa3.

---
+++ axcend_focus_front_panel_button/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+package_name = 'axcend_focus_front_panel_button'
+
+setup(
+    name=package_name,
+    version='3.1.3',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='ubuntu',
+    maintainer_email='graham.harrison@axcendcorp.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'front_panel_button_controller = axcend_focus_front_panel_button.front_panel_button_controller:main'
+        ],
+    },
+)
---
+++ axcend_focus_launch/launch/application_launch.py
@@ -0,0 +1,16 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    return LaunchDescription([
+        Node(
+            package='axcend_focus_ros2_firmware_bridge',
+            executable='firmware_bridge',
+            name='firmware_bridge'
+        ),
+        Node(
+            package='axcend_focus_legacy_compatibility_layer',
+            executable='legacy_compatibility_interface',
+            name='legacy_compatibility_interface'
+        )
+    ])
