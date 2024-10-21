1cf4b1a7b51bd04d330dfd25b33a8f39877aadcc
Graham Harrison <graham.harrison@axcendcorp.com>
2024-10-21 15:44:44 -0600

    Revert "Trimmed the branch to only the axcend_focus_ros2_firmware_bridge sub directory"
    This reverts commit a05473e2b3530cebeb29b2080215bd05a8027737.

---
+++ axcend_focus_test_utils_package/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+package_name = 'axcend_focus_test_utils_package'
+
+setup(
+    name=package_name,
+    version='3.1.7',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='ubuntu',
+    maintainer_email='graham.harrison@axcendcorp.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)
