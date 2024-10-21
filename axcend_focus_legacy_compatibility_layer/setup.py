e57b6d9cdbb3eeb3341685947fa8d5d252c3820b
Graham Harrison <graham.harrison@axcendcorp.com>
2024-10-21 14:39:55 -0600

    Rebase from 'upstream'

--- axcend_focus_legacy_compatibility_layer/setup.py
+++ axcend_focus_legacy_compatibility_layer/setup.py
@@ -4,7 +4,7 @@ package_name = 'axcend_focus_legacy_compatibility_layer'
 
 setup(
     name=package_name,
-    version='3.1.6',
+    version='3.1.7',
     packages=[package_name],
     data_files=[
         ('share/ament_index/resource_index/packages',
